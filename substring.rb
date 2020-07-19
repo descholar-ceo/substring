92d1738dc24f38de9242f31acdb101a0fac70efa
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-19 22:32:17 +0200

    Add substring method

--- substring.rb
+++ substring.rb
@@ -1,1 +1,8 @@
-p 'hello world'
+def substring(word, dictionary)
+    
+end
+
+dictionary = %w[below down go going horn how howdy it i low own part partnersit]
+word = 'below'
+
+p substring(word, dictionary)
