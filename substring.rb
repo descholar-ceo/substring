b5807bc2d6ff28e98dcbb32e9bd911a87dfed88a
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-19 23:11:28 +0200

    Add logic

--- substring.rb
+++ substring.rb
@@ -1,8 +1,17 @@
 def substring(word, dictionary)
-    
+  result = Hash.new
+  counter = 0
+  dictionary.each do |current_word|
+    if word.include? current_word
+        counter += 1
+        result[current_word] = counter
+        # counter = 0
+    end
+  end
+  return result
 end
 
-dictionary = %w[below down go going horn how howdy it i low own part partnersit]
+dictionary = %w[below down go going horn how howdy it i low own part partnersit low below]
 word = 'below'
 
 p substring(word, dictionary)
