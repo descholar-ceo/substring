d4cca9af9fcdb8483b752a5fe9db90de422e0686
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-19 23:43:34 +0200

    add substring multiple method

--- substring.rb
+++ substring.rb
@@ -9,7 +9,21 @@ def substring(word, dictionary)
   result
 end
 
-dictionary = %w[below down go going horn how howdy it i low own part partner sit ]
+def substring_multiple(sentence, dictionary)
+  result = {}
+  word_arr = sentence.split(' ')
+  word_arr.each do |word|
+    dictionary.each do |current_word|
+      if word.include? current_word
+        occurency = dictionary.count(current_word)
+        result[current_word] = occurency
+      end
+    end
+  end
+  result
+end
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 word = "Howdy partner, sit down! How's it going?"
 
-p substring(word, dictionary)
+p substring_multiple(word, dictionary)
