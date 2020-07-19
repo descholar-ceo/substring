e795cf78bda461b5837c9b306e03113c59f7e84e
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-19 23:21:24 +0200

    finish the method substring

--- substring.rb
+++ substring.rb
@@ -1,17 +1,15 @@
 def substring(word, dictionary)
-  result = Hash.new
-  counter = 0
+  result = {}
   dictionary.each do |current_word|
     if word.include? current_word
-        counter += 1
-        result[current_word] = counter
-        # counter = 0
+      occurency = dictionary.count(current_word)
+      result[current_word] = occurency
     end
   end
-  return result
+  result
 end
 
-dictionary = %w[below down go going horn how howdy it i low own part partnersit low below]
+dictionary = %w[below down go going horn how howdy it i low own part partnersit low]
 word = 'below'
 
 p substring(word, dictionary)
